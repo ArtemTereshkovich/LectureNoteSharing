afec06dcaf6c2aaf44ccff40ab0f4cd67e5b5e76
ITRANSITION\a.tereshkovich <a.tereshkovich@itransition.com>
2018-10-25 20:19:56 +0300

    Add header to app component and add Main page(but dont resolve in routing)

--- src/Client/e2e/src/app.po.ts
+++ src/Client/e2e/src/app.po.ts
@@ -5,7 +5,7 @@ export class AppPage {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getNavbarMainElementText() {
+    return element(by.id('navbar-main-elelement')).getText();
   }
 }
