0f3a4c10f57ce22c87469e7a948b074fa0b89dd7
ITRANSITION\a.tereshkovich <a.tereshkovich@itransition.com>
2018-11-01 10:16:48 +0300

    Fix test bug at lecturenotes-list

--- src/Client/src/app/shared/lecturenotes/lecturenotes-list/lecturenotes-list.component.spec.ts
+++ src/Client/src/app/shared/lecturenotes/lecturenotes-list/lecturenotes-list.component.spec.ts
@@ -1,23 +1,42 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LecturenotesListComponent } from './lecturenotes-list.component';
 import { PaginationModule } from 'ngx-bootstrap';
+import { LecturenotePreviewComponent } from '../lecturenote-preview/lecturenote-preview.component';
+import { FormsModule } from '@angular/forms';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { LectureNotesListConfig } from './lecutrenotes-list.config';
+
+@Component({
+  selector  : 'app-test-cmp',
+  template  : '<app-lecturenotes-list [defaultConfig]="mockDefaultConfig"></app-lecturenotes-list>'
+ })
+ class TestWrapperComponent {
+     mockDefaultConfig = new LectureNotesListConfig(); // mock your input
+ }
 
 describe('LecturenotesListComponent', () => {
   let component: LecturenotesListComponent;
-  let fixture: ComponentFixture<LecturenotesListComponent>;
+  let fixture: ComponentFixture<TestWrapperComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ PaginationModule ],
-      declarations: [ LecturenotesListComponent ]
+      imports: [
+        PaginationModule.forRoot(),
+        FormsModule
+       ],
+      declarations: [
+        TestWrapperComponent,
+        LecturenotesListComponent,
+        LecturenotePreviewComponent
+     ],
+     schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LecturenotesListComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TestWrapperComponent);
+    component = fixture.debugElement.children[0].componentInstance;
     fixture.detectChanges();
   });
 
@@ -25,3 +44,4 @@ describe('LecturenotesListComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
