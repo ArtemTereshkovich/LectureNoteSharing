a45cd88e2200b3a3cef797d9480eccf7dc5557c9
Артём Терешкович <artyomt7777@gmail.com>
2018-11-09 18:03:00 +0300

    add path convention

--- src/Client/src/app/app.module.ts
+++ src/Client/src/app/app.module.ts
@@ -7,12 +7,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeModule } from './home/home.module';
 import { RouterModule } from '@angular/router';
 import { JWTService } from './shared/services/jwt.service';
-import { SharedModule } from './shared/shared.module';
 import { LectureNotesServiceMock } from './shared/mocks/lecturenotesservice.mock';
 import { LectureNoteTagServiceMock } from './shared/mocks/lecturenotetagservice.mock';
 import { LectureNoteRatingServiceMock } from './shared/mocks/lecturenoteratingservice.mock';
 import { FormsModule } from '@angular/forms';
-import { CreateComponent } from './create/create.component';
 import { CreateModule } from './create/create.module';
 
 const rootRouting: ModuleWithProviders = RouterModule.forRoot([], { useHash: true });
